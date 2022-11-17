9b9d1de34624299e672f460d0378d79bf07c0629
Isaac0312 <75089817+Isaac0312@users.noreply.github.com>
2022-11-17 09:37:43 +0100

    Add files via upload

---
+++ CientificosT26/src/main/java/com/example/demo/CientificosT26Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CientificosT26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CientificosT26Application.class, args);
+	}
+
+}
