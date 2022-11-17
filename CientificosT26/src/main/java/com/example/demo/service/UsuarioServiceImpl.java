6de3272559372cc999ae118630f53c3ff803cc46
Isaac0312 <isaacejarque@hotmail.com>
2022-11-17 21:48:50 +0100

    Commit haciendo seguridad

---
+++ CientificosT26/src/main/java/com/example/demo/service/UsuarioServiceImpl.java
@@ -0,0 +1,32 @@
+package com.example.demo.service;
+import static java.util.Collections.emptyList;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import com.example.demo.dto.Usuario;
+import com.example.demo.dao.IUsuarioDAO;
+
+
+
+@Service
+public class UsuarioServiceImpl implements UserDetailsService {
+
+	private IUsuarioDAO iUsuarioDAO;
+
+	public UsuarioServiceImpl(IUsuarioDAO iUsuarioDAO) {
+		this.iUsuarioDAO = iUsuarioDAO;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Usuario usuario = iUsuarioDAO.findByUsername(username);
+		if (usuario == null) {
+			throw new UsernameNotFoundException(username);
+		}
+		return new User(usuario.getUsername(), usuario.getPassword(), emptyList());
+	}
+	
+}
